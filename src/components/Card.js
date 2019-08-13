2a236abdff2b6e0645a011a9326cb71d424dcaee
Mafalda <mafalda.villazan@gmail.com>
2019-08-12 19:43:45 -0700

    Creating a function that can generate a random number

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,10 +7,40 @@ function Card() {
                 <h3>Card title</h3>
             </div>
             <div className="card-body">
-                <p className="card-text">Card content</p>
+                < Random/>
             </div>
         </div>
     );
+}
+
+//Creating a class that changes from something to random
+class Random extends React.Component {
+    state = {
+        count: 0
+    };
+    selectRandom = () => {
+        // We always use the setState method to update a component's state
+        this.setState({ count: Math.floor((Math.random() * 10) + 1) });
+        
+    };
+
+
+    // The render method returns the JSX that should be rendered
+    render() {
+        return (
+            <div className="card text-center">
+                <div className="card-header bg-primary text-white">
+                    Click Counter!
+                </div>
+                <div className="card-body">
+                    <p className="card-text">Click Count: {this.state.count}</p>
+                    <button className="btn btn-primary" onClick={this.selectRandom}>
+                        Increment
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Card;
