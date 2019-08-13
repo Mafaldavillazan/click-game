0015b89c6a070f6cbf0883f53ca1c9735c1e391e
Mafalda <mafalda.villazan@gmail.com>
2019-08-13 15:34:12 -0700

    Adding score when the user clicks an image

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,22 +4,27 @@ import ImageCard from "./ImageCard";
 import Wrapper from "./Wrapper";
 
 
-
 //Creating a class that changes from something to random
 class CardDisplay extends React.Component {
     state = {
-        imagesData
+        imagesData,
+        count:0
     };
-    // selectRandom = () => {
-    //     // We always use the setState method to update a component's state
-    //     this.setState({ count: Math.floor((Math.random() * 10) + 1) });  
-    // };
+
+    //Every time you click it out increments
+    addScore = () => {
+        // We always use the setState method to update a component's state
+        this.setState({ count: this.state.count + 1 });
+        console.log(this.state.count)
+    };
+
     render() {
         return (
           <Wrapper>
             {this.state.imagesData.map(results => (
               <ImageCard
                 {...results}
+                onClick={this.addScore}
                 />
             ))}
           </Wrapper>
--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function ImageCard(props) {
   return (
     <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+      <div className="img-container"  onClick={props.onClick}>
+        <img alt={props.name} src={props.image}/>
       </div>
     </div>
   );
